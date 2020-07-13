947c570792ee06be5e9465020a44e3dd5df10290
娜样刚好 <37099559+ZhaoGang2018@users.noreply.github.com>
2020-07-13 11:09:36 +0800

    Add files via upload

---
+++ SwiftSummary/SwiftSummary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSummary
+//
+//  Created by jing_mac on 2020/7/13.
+//  Copyright © 2020 zhaogang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftSummary/SwiftSummaryTests/SwiftSummaryTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftSummaryTests.swift
+//  SwiftSummaryTests
+//
+//  Created by jing_mac on 2020/7/13.
+//  Copyright © 2020 zhaogang. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftSummary
+
+class SwiftSummaryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
